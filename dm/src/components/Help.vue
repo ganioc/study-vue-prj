ce29cd148f1c83a3ea50f2ac63020a0986715d06
yango3 <o3.yang@hotmail.com>
2019-12-02 17:11:57 +0800

    Add new dm prj

---
+++ dm/src/components/Help.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="help">
+    <h1>Help</h1>
+    <h2>{{ msg}}</h2>
+    <Button>success</Button>
+    <h3>
+      <a href="/">Home</a>
+    </h3>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Help',
+  data() {
+    return {
+      msg: 'Help app'
+    }
+  }
+}
+</script>
+<style>
+</style>
